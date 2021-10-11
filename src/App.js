34561c68775102a72d02cdfa4a9782c8023ca34c
Clancinio <dean.clancy91@gmail.com>
2021-10-11 13:07:37 +0100

    Map appointments to list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { BiArchive } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointment";
+import ListGroup from "react-bootstrap/ListGroup";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+import appointmentList from "./data.json";
 
 function App() {
   return (
@@ -10,6 +15,24 @@ function App() {
       </h1>
       <AddAppointment />
       <Search />
+      <ListGroup>
+        {appointmentList.map((appoinment) => (
+          <ListGroup.Item>
+            <Card>
+              <Card.Header>{appoinment.petName}</Card.Header>
+              <Card.Body>
+                <Card.Title>Owner: </Card.Title>
+                <Card.Text>{appoinment.ownerName}</Card.Text>
+                <Card.Title>Appointment Date: </Card.Title>
+                <Card.Text>{appoinment.aptDate}</Card.Text>
+                <Card.Title>Notes:: </Card.Title>
+                <Card.Text>{appoinment.aptNotes}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
   );
 }
