aaa83c9d17647f158b7cef5614618757b402102a
Clancinio <dean.clancy91@gmail.com>
2021-10-11 12:46:11 +0100

    Add date and time field

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BiArchive } from "react-icons/bi";
 import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
 
 function App() {
   return (
-    <div className="container mt-3">
-      <h1 className="text-5xl">
+    <div className="container mt-3 ">
+      <h1 className="mb-5">
         <BiArchive className="" /> Your appointments
       </h1>
+      <AddAppointment />
       <Search />
     </div>
   );
--- src/components/AddAppointment.js
+++ src/components/AddAppointment.js
@@ -1,7 +1,60 @@
-import { BiCalendarPlus } from "react-icons/bi";
+// import { BiCalendarPlus } from "react-icons/bi";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function Search() {
-  return <h1>Add</h1>;
+function AddComponent() {
+  return (
+    <Form className="mb-5 p-3 border border-secondary rounded">
+      <Row className="form-header">
+        <h2>Add Appointment</h2>
+      </Row>
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+        <Form.Label column sm="2">
+          Owner Name
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="text" />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+        <Form.Label column sm="2">
+          Pet Name
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="text" />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+        <Form.Label column sm="2">
+          Date
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="date" />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+        <Form.Label column sm="2">
+          Date
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="time" />
+        </Col>
+      </Form.Group>
+
+      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+        <Form.Label column sm="2">
+          Appointment notes
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control as="textarea" rows={3} />
+        </Col>
+      </Form.Group>
+    </Form>
+  );
 }
 
-export default Search;
+export default AddComponent;
